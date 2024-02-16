82800f38c876d76821f9c041d42f3cdc9c4e59ec
nexola <vitormatheusfv@gmail.com>
2024-02-16 16:54:12 -0300

    GET User by id

--- src/main/java/com/nexola/workshopmongodb/controllers/UserController.java
+++ src/main/java/com/nexola/workshopmongodb/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.nexola.workshopmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,11 @@ public class UserController {
     public ResponseEntity<List<UserDTO>> findAll() {
         List<UserDTO> dto = service.findAll();
         return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        UserDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
     }
 }
--- src/main/java/com/nexola/workshopmongodb/services/UserService.java
+++ src/main/java/com/nexola/workshopmongodb/services/UserService.java
@@ -3,6 +3,7 @@ package com.nexola.workshopmongodb.services;
 import com.nexola.workshopmongodb.models.dto.UserDTO;
 import com.nexola.workshopmongodb.models.entities.User;
 import com.nexola.workshopmongodb.repositories.UserRepository;
+import com.nexola.workshopmongodb.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,5 +21,13 @@ public class UserService {
     public List<UserDTO> findAll() {
         List<User> result = repository.findAll();
         return result.stream().map(UserDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO findById(String id) {
+        User user = repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Recurso não encontrado")
+        );
+        return new UserDTO(user);
     }
 }
